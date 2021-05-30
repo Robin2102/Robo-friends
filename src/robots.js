813916a59c688c8062d1c1658fc88cfeb0c266f9
Robin2102 <84293796+Robin2102@users.noreply.github.com>
2021-05-31 03:32:07 +0530

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,71 @@
+export const robots = [
+  {
+  	id: 1,
+  	name: 'Leanne Graham',
+  	username: 'Bret',
+  	email: 'Sincere@april.biz'
+  },
+
+  {
+  	id: 2,
+  	name: 'Erwin Howell',
+  	username: 'Antonette',
+  	email: 'Shanna@mellisa.tv'
+  },
+
+  {
+  	id: 3,
+  	name: 'Clementine Bauch',
+  	username: 'Samantha',
+  	email: 'Nathan@yesenia.net'
+  },
+
+  {
+  	id: 4,
+  	name: 'Patricia Lesbek',
+  	username: 'Karianne',
+  	email: 'Julianne.OConner@kory.org'
+  },
+
+  {
+  	id: 5,
+  	name: 'Chelsey Deltrich',
+  	username: 'Kamren',
+  	email: 'Shinna@yellisa.tc'
+  },
+  
+  {
+  	id: 6,
+  	name: 'Mrs. Domnis cartel',
+  	username: 'Charmele',
+  	email: 'Charmele@andrie.info'
+  },
+  
+  {
+  	id: 7,
+  	name: 'Kurtis Wismette',
+  	username: 'Wismette',
+  	email: 'Telly.hoeger@billy.biz'
+  },
+  
+  {
+  	id: 8,
+  	name: 'Nicholoas Rumolfsdottir V',
+  	username: 'Maxime_Nienow',
+  	email: 'Sherwood@rosemand.me'
+  },
+  
+  {
+  	id: 9,
+  	name: 'Glenna Richard',
+  	username: 'Dolphine',
+  	email: 'chain@meliqua.io'
+  },
+  
+  {
+  	id: 10,
+  	name: 'Clementina Dubuque',
+  	username: 'Noriah.Stanton',
+  	email: 'Rey.Padberg@karina.biz'
+  }, 
+]
